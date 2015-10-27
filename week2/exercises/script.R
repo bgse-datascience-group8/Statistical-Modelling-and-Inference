775550a527966e7998d59322355cdeda76cfbe1e
rogercusco <rogercusco@gmail.com>
2015-10-27 01:24:23 +0100

    Added draft answers for MT project exs 2,3,4

--- week2/exercises/script.R
+++ week2/exercises/script.R
@@ -1,49 +1,62 @@
-library(MASS)
+A <- matrix( c(3,2,1,2,4,1,1,1,5) , 3 , 3 )
 
-constructPhi <- function(features, M, basis_type) {
-  phi <- matrix(nrow = length(xs), ncol = M + 1)
-    # Construct a phi matrix that is phix for each value of phix(x, M, 'Gauss') and append to phi matrix
-  for (i in 1:(length(xs))) {
-    phi[i,] = phix(xs[i], M, basis_type)
+L <- matrix(0, nrow(A), ncol(A))
+
+L[1,1] <- sqrt( A[1,1] )
+L[2,1] <- 1/(L[1,1]) * A[2,1]
+L[2,2] <- sqrt( A[2,2] - L[2,1]**2 )
+L[3,1] <- 1/(L[1,1]) * ( A[3,1] )
+L[3,2] <- 1/(L[2,2]) * ( A[3,2] - L[3,1]*L[2,1] )
+L[3,3] <- sqrt( A[3,3] - L[3,1]**2 - L[3,2]**2 )
+
+round(L,2)
+round(t(chol(A)),2)
+
+choleski <- function(A) {
+  for (j in 1:ncol(A)) {  
+    for (i in 1:nrow(A)) {
+      
+      ii_sum <- function(i,j) {
+        result = 0
+        for (e in 1:(i-1)) {
+          result = result + L[i,e]**2
+        }  
+      } 
+      
+      ij_sum <- function(i,j) {
+        result2 = 0
+        for (r in 1:(i-2)) {
+          result2 = result2 + (L[r,1])*(L[r-1,1])
+        }
+      }
+      
+      if (i==j) {
+        L[i,j] = sqrt( A[i,j] - ii_sum(i,j))
+      } 
+      if (i < j) {
+        L[i,j] = 1/(L[j,j])*(A[i,j] - ij_sum(i,j))
+      } else {
+        L[i,j] = 0
+      }
+  return(L)
   }
-  phi
+ }
 }
 
-# Part 3: post.params
-post.params <- function(training_data, M, basis_type, delta, q) {
-  # regularization coefficient
-  # training_data = curve_data
-  # q = (1/0.1)^2
-  # delta = 2.0
-  # basis_type = 'Gauss'
-  lambda = delta / q
 
-  # Note: this requires the training data to have an x column
-  xs = training_data$x
-  # M = 9
-  phi <- constructPhi(xs, M, basis_type)
-  t = training_data$t
-  
-  # Slide 10 of Bayessian_regression.pdf
-  w_Bayes = ginv(lambda * diag(M+1) + t(phi) %*% phi) %*% t(phi) %*% training_data$t
+ii_sum <- function(i,j) {
+  result = 0
+  for (e in 1:(i-1) {
+    result = result + L[i,e]**2
+  }  
+} 
 
-  D = delta * diag(M + 1)
-  Q = D + q * (t(phi) %*% phi)
-
-  # FIXME: RETURN Q?
-  list(w_Bayes, Q)
+ij_sum <- function(i,j) {
+  result2 = 0
+  for (r in 1:i) {
+    result2 = result2 +(L[i,e+1])*(L[i,e])
+  }
 }
 
-M = 9
-delta = 2.0
-q = (1/0.1)^2
 
-params <- post.params(curve_data, M, 'poly', delta, q)
-w_Bayes <- params[[1]]
-Q <- params[[2]]
-result <- constructPhi(curve_data$x, M, 'poly') %*% w_Bayes
 
-# Part 4: Plot
-plot(curve_data, col = 'red')
-# Don't know how to smooth this out :(
-lines(y = result, x = curve_data$x, type = 'l', col = 'blue')
